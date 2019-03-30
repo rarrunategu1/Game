6c3f6376410246ce2f9a135a8bf8939547f4d405
rarrunategu1 <parker.rose@ymail.com>
2019-03-29 21:54:23 -0400

    drawboard beginnings in c#.   working on adding a drawboard character field in the drawboard

--- game/Program.cs
+++ game/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace game
 {
-    class Program
+    public class drawBoard
     {
+        //string drawboardChar = " ";
+        char width = '|';
+        char height = '-';
+        
+
         static void Main(string[] args)
         {
+            drawBoard db = new drawBoard();
+            db.draw_board(6, 3);
+            
         }
+
+        private void draw_board(int x, int y)
+        {
+            //Column
+            for (int j = 0; j < y; j++)
+            {
+               
+                string widthPrint = new String(width ,x);
+                string rowPrint = new String(height, x);
+                
+                Console.WriteLine(widthPrint);
+                Console.WriteLine(rowPrint);
+                
+                //Row
+                for (int i = 0; i < x; i++)
+                {    
+                       // string widthPrint = new String(width ,x);
+                       // string rowPrint = new string('-', y);
+                       // Console.WriteLine(widthPrint);
+                       // Console.WriteLine(rowPrint);
+                        //Console.ReadLine();
+                        
+                    
+                }
+                //Console.ReadLine();
+
+            }  
+            Console.ReadLine();
+        }
+
+
     }
 }
